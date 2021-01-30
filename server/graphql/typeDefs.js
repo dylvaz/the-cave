0504cad0020e2216920ba56238f96aaf979a39dc
dylvaz <vazquezadylan@gmail.com>
2021-01-30 10:47:04 -0800

    implemented additional fields to graphql schema - added comments, likes, likeCount, and commentCount to Post type - added Comment and Like types - added createComment, deleteComment, and likePost mutations

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -6,6 +6,21 @@ const { gql } = require('apollo-server');
     body: String!
     username: String!
     createdAt: String!
+    comments: [Comment]!
+    likes: [Like]!
+    likeCount: Int!
+    commentCount: Int!
+  }
+  type Comment {
+    id: ID!
+    body: String!
+    username: String!
+    createdAt: String!
+  }
+  type Like {
+    id: ID!
+    username: String!
+    createdAt: String!
   }
   type User {
     id: ID!
@@ -29,5 +44,8 @@ const { gql } = require('apollo-server');
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: String!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
