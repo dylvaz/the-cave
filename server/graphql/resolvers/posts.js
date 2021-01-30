e64464296f74a2a8f2614d296daf129b6cfac115
dylvaz <vazquezadylan@gmail.com>
2021-01-30 10:44:55 -0800

    implemented likePost mutation to unlike and like posts - validates user jwt token using checkAuth - checks to see if user has already like post or not - likes or unlikes depending on whether or not the user already liked the post

--- server/graphql/resolvers/posts.js
+++ server/graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/checkAuth');
 
@@ -41,6 +41,7 @@ module.exports = {
     },
     deletePost: async (_, { postId }, context) => {
       const user = checkAuth(context);
+      
       try {
         const post = await Post.findById(postId);
         if (user.username === post.username) {
@@ -48,6 +49,29 @@ module.exports = {
           return 'Post deleted succesfully.';
         } else {
           throw new AuthenticationError('Action not allowed.');
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    likePost: async (_, { postId }, context) => {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          if (post.likes.find(like => like.username === user.username)) {
+            post.likes = post.likes.filter(like => like.username !== user.username);
+          } else {
+            post.likes.push({
+              username: user.username,
+              createdAt: new Date().toISOString()
+            });
+          }
+          await post.save();
+          return post;
+        } else {
+          throw new UserInputError('Post not found.');
         }
       } catch (err) {
         throw new Error(err);
