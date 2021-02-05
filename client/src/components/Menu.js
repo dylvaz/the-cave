25e2524f0be131feb7113611dc7524282a913c6a
dylvaz <vazquezadylan@gmail.com>
2021-02-05 03:35:36 -0800

    implemented a createPost tab on the menuBar and fixed styling

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -7,18 +7,26 @@ import { AuthContext } from "../context/auth";
 const MenuBar = () => {
   const { user, logout } = useContext(AuthContext);
   const pathname = window.location.pathname;
-  const path= pathname === '/' ? 'home' : pathname.substr(1);
+  const path = pathname === '/' ? 'home' : pathname.substr(1);
   const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
-
-  const menuBar = user ? (
+  
+  const menuBar = user ? ( 
     <Menu pointing secondary size="massive" color="purple">
-      <Menu.Item
+      <Menu.Item style={{ textTransform: 'lowercase' }}
         name={user.username}
-        active
+        active={activeItem === user.username || pathname === '/'}
+        onClick={handleItemClick}
         as={Link}
         to='/'
+      />
+      <Menu.Item
+        name='Create Post'
+        active={activeItem === 'Create Post' || pathname === '/createPost'}
+        onClick={handleItemClick}
+        as={Link}
+        to='/createPost'
       />
       <Menu.Menu position='right'>
         <Menu.Item
@@ -29,33 +37,33 @@ const MenuBar = () => {
         />
       </Menu.Menu>
     </Menu>
-  ): (
-    <Menu pointing secondary size="massive" color="purple">
-      <Menu.Item
-        name='home'
-        active={activeItem === 'home'}
-        onClick={handleItemClick}
-        as={Link}
-        to='/'
-      />
-      <Menu.Menu position='right'>
+  ) : (
+      <Menu pointing secondary size="massive" color="purple">
         <Menu.Item
-          name='login'
-          active={activeItem === 'login'}
+          name='home'
+          active={activeItem === 'home' || pathname === '/' }
           onClick={handleItemClick}
           as={Link}
-          to='/login'
+          to='/'
         />
-        <Menu.Item
-          name='register'
-          active={activeItem === 'register'}
-          onClick={handleItemClick}
-          as={Link}
-          to='/register'
-        />
-      </Menu.Menu>
-    </Menu>
-  )
+        <Menu.Menu position='right'>
+          <Menu.Item
+            name='login'
+            active={activeItem === 'login'}
+            onClick={handleItemClick}
+            as={Link}
+            to='/login'
+          />
+          <Menu.Item
+            name='register'
+            active={activeItem === 'register'}
+            onClick={handleItemClick}
+            as={Link}
+            to='/register'
+          />
+        </Menu.Menu>
+      </Menu>
+    );
 
   return menuBar;
 };
