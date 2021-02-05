25d8829b387d3a432b963e471c1622e21bbe13a7
dylvaz <vazquezadylan@gmail.com>
2021-02-04 19:38:15 -0800

    implemented setcontext from apollo/client/link/context
    - retrieves token if it exists and then adds it to headers if it does

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { createHttpLink } from 'apollo-link-http';
+import { setContext } from '@apollo/client/link/context';
+
 
 import App from './App';
 
@@ -8,8 +10,20 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:5000/'
 });
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('jwtToken');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
