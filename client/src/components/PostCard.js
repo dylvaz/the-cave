eea014f9108faa022f88810e99d60d81b7774480
dylvaz <vazquezadylan@gmail.com>
2021-02-09 00:07:59 -0800

    refactored deleteButton to it's own component

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
 
 const PostCard = ({ post: { body, createdAt, id, username, likeCount, commentCount, likes } }) => {
   const { user } = useContext(AuthContext);
@@ -31,11 +32,7 @@ const PostCard = ({ post: { body, createdAt, id, username, likeCount, commentCou
             {commentCount}
           </Label>
         </Button>
-        {user && user.username === username && (
-          <Button as='div' color='red' floated='right' onClick={() => console.log('Delete Post')}>
-            <Icon name='trash' style={{margin: 0}} />
-          </Button>
-        )}
+        {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
   );
