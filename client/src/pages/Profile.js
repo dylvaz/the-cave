f52bac294d1ebc08267eb7e010397dc7dbf1b2e3
dylvaz <vazquezadylan@gmail.com>
2021-02-23 20:59:34 -0800

    implemented useEffect hook to watch the state of user

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,24 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Card, Icon, Image } from 'semantic-ui-react';
 import { parseISO } from 'date-fns';
+import PFP from '../assets/defaultPFP.jpg';
 
 import { AuthContext } from '../context/auth';
 
 const Profile = () => {
-
   const { user } = useContext(AuthContext);
-  
+
+  useEffect(() => {}, [user]);
+
   return (
     <Card>
-      <Image src='/images/avatar/large/matthew.png' />
+      {console.log(user)}
+      <Image src={user.imgUrl || PFP} />
       <Card.Content>
         <Card.Header>{user.username}</Card.Header>
         <Card.Meta>
           <span>Joined on {parseISO(user.createdAt).toDateString()}</span>
         </Card.Meta>
-        <Card.Description>
-
-        </Card.Description>
+        <Card.Description></Card.Description>
       </Card.Content>
       <Card.Content extra>
         <Icon name='user' />
