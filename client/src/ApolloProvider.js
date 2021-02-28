d4259d20e0efeb64abd70d37a0c6a0c78a99b64b
dylvaz <vazquezadylan@gmail.com>
2021-02-28 00:06:47 -0800

    removed localhosturi from apollo client so it will query the database that heroku gives it

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -6,7 +6,6 @@ import { createUploadLink } from 'apollo-upload-client';
 import App from './App';
 
 const uploadLink = createUploadLink({
-  uri: 'http://localhost:5000',
   headers: {
     'keep-alive': 'true',
   },
